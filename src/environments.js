3a3e2e7b22ecc102946d2f181ba828ea22f66a34
Ryan Pairitz <rpairitz@nd.edu>
2021-10-27 19:33:38 -0400

    installed and initialized Parse SDK, with API keys from Back4App

---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: "Yq7OkGaf7slgd2KEiulBagQikpsWB81SYXWkcSmM",
+  JAVASCRIPT_KEY: "HshvqhRlH2qpFxwIIBaVfQuWu4YKnRKFoRbSz3RX",
+  SERVER_URL: "CZfMXUj1HywZHlRTBbetGulClo86krADITSXLxnH"
+};
