25c5fe96d8567c90bc6628f5a467057fe6e6e903
Ryan Pairitz <rpairitz@nd.edu>
2021-10-27 23:48:53 -0400

    load asynch data for Products using Parse

--- src/Components/Main/ProductList.js
+++ src/Components/Main/ProductList.js
@@ -1,11 +1,40 @@
-/*
 import {
     getAllProducts
-} from "/src/Services/ShopService";
-*/
+} from "../../Services/ShopService.js";
+import {useState,useEffect} from "react";
 
+// stateful parent component for rendering Product data down to child
 export function ProductList () {
+    // variable in state to hold array of Products; initial state: empty
+    const [products, setProducts] = useState([]);
+
+    // when page loads, run useEffect to get asynchronous data to render
+    useEffect(()=> {
+        getAllProducts().then((products) => {
+            console.log(products);
+            setProducts(products);
+        });
+    }, []);
+
     return (
-        <p> Within ProductList component</p>
+        <div>
+            <p> Within ProductList component</p>
+            <div>
+                {products.length > 0 && (
+                    <ul>
+                        {products.map((product) => ( 
+                            <div>
+                                <span>
+                                    {/* display product name; display image with styling later */}
+                                    <li key={product.id}>{product.get("name")}</li>{""}
+                                    {/* want to add button to add product to cart/checkout later (in a separate service for Checkout) */}
+                                    <button>Add to Cart</button>
+                                </span>
+                            </div>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
     );
 };
