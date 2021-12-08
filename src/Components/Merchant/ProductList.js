946a19c5e7af2b006da0ea3049d16951d80b521b
Ryan Pairitz <rpairitz@nd.edu>
2021-12-07 23:27:39 -0500

    add delete function/button to delete items

--- src/Components/Merchant/ProductList.js
+++ src/Components/Merchant/ProductList.js
@@ -1,4 +1,4 @@
-import { getProductsByMerchant } from "../../Services/ShopService.js";
+import { getProductsByMerchant, removeProduct } from "../../Services/ShopService.js";
 import {useState,useEffect} from "react";
 import ProtectedRoute from "../../Common/ProtectedRoute.js";
 import Parse from "parse";
@@ -10,16 +10,35 @@ const ProductList = () => {
 
     // variable in state to hold array of Products; initial state: empty
     const [products, setProducts] = useState([]);
+    // flag in the state to watch for delete update
+    const [removeId, setRemoveId] = useState('');
 
     // when page loads, run useEffect to get asynchronous data to render
     useEffect(()=> {
         if (currentUser){
             getProductsByMerchant(currentUser.id).then((products) => {
-                console.log(products);
                 setProducts(products);
             });
         }
     }, [currentUser]);
+
+    // separate useEffect for deleting items and updating the product list state
+    useEffect(() => {
+        if (removeId){
+            console.log(removeId);
+            removeProduct(removeId).then(() => {
+                setRemoveId('');
+                getProductsByMerchant(currentUser.id).then((products) => {
+                    setProducts(products);
+                });
+            });
+        }
+    }, [removeId]);
+
+    const removeHandler = (id) => {
+        console.log('removeHandler');
+        setRemoveId(id);
+    };
 
     if (!currentUser){
         return (
@@ -40,7 +59,8 @@ const ProductList = () => {
                                         <h4 key={product.id}><Link key="link" to={{pathname: `/Product/${product.id}`, state: {product: product}}}> {product.get("name")} </Link></h4>
                                         <img key="img" src={product.get("image")._url} alt="" style={{width: "250px"}}/>
                                         <br key="br"/>
-                                        <button key="button">Add to Cart</button>
+                                        <button key="button">Edit Item</button>
+                                        <button key="button" onClick={() => removeHandler(product.id)}>Delete Item</button>
                                 </div>    
                         ))}
                     </div>
